d5d3e0f8b557a1b89018fd3c8e7b014c4ba0f8c8
ciastkoMalinowe <olamnich@gmail.com>
2017-12-11 17:15:36 +0100

    simple filters + searching for alpha

--- lab_examples/face_detection.py
+++ lab_examples/face_detection.py
@@ -3,8 +3,9 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
-cat = cv2.imread('cat.jpg', -1)
-small_cat = cat
+cat = cv2.imread('cat.png', -1)
+mask = cv2.imread('cat.png', 0)
+small_cat = cv2.add(np.zeros((cat.shape[0],cat.shape[1],3), np.uint8), cat, mask=mask)
 while(True):
     ret, frame = cap.read()
     
@@ -22,7 +23,10 @@ while(True):
         cv2.rectangle(frame, (x*10,y*10), (w*10+x*10, y*10+h*10), (255, 255, 0), 2)
         #cv2.rectangle(small_frame, (x,y), (w+x, y+h), (255, 255, 0), 2)
         small_cat = cv2.resize(cat, (0,0), fx=(w*10/cat.shape[0]), fy=(h*10/cat.shape[1]))
-        frame[y*10:y*10+small_cat.shape[0], x*10:x*10+small_cat.shape[1]] = small_cat
+        small_mask = cv2.resize(mask, (0,0), fx=(w*10/cat.shape[0]), fy=(h*10/cat.shape[1]))
+        frame[y*10:y*10+small_cat.shape[0], x*10:x*10+small_cat.shape[1]] = \
+            cv2.add(frame[y*10:y*10+small_cat.shape[0],
+                    x*10:x*10+small_cat.shape[1]],small_cat, mask = small_mask)
     #cv2.imshow('CAT', small_cat)
     cv2.imshow('FACES',frame)
     cv2.namedWindow('FACES', cv2.WINDOW_NORMAL)
---
+++ lab_examples/filters.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+vertical_edges = np.array([[0,0,-1,0,0],[0,0,-1,0,0],[0,0,4,0,0],[0,0,-1,0,0],[0,0,-1,0,0]])
+horizontal_edges = np.array([[0,0,-1,0,0],[0,0,-1,0,0],[0,0,2,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+edges45 = np.array([[-1,0,0,0,0],[0,-2,0,0,0],[0,0,6,0,0],[0,0,0,-2,0],[0,0,0,0,-1]])
+laplace = np.array([[-1, -1, -1], [-1,  8, -1], [-1, -1, -1]])
+sharpen = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+excess_edges = np.array([[1,1,1],[1,-7,1],[1,1,1]])
+gauss = np.array([[1, 1, 2, 1, 1], [1, 2, 4, 2, 1], [2, 4, 8, 4, 2], [1, 2, 4, 2, 1], [1, 1, 2, 1, 1]], np.float32) / 52
+
+while (True):
+    ret, frame = cap.read()
+    filtered = cv2.filter2D(frame, -1, edges45)
+
+    cv2.imshow('FILTERED', filtered)
+    cv2.imshow('TEST', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
